2e490a6b54555b71cad99b4aff30ed26b840e657
Marko Bakovic <markobakovic95@gmail.com>
2017-07-28 01:17:20 +0200

    nearest neighbor, demo folder

--- demo/main.py
+++ demo/main.py
@@ -3,3 +3,11 @@
 # 2. vector to image
 # 3. image to vector
 # 4. load image and edit it with vectors
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
