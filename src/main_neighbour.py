feb992ac0b3ce3c70146ee72076661360d47864f
Marko Bakovic <markobakovic95@gmail.com>
2017-07-28 11:28:06 +0200

    neighbour playground todos

---
+++ src/main_neighbour.py
@@ -0,0 +1,6 @@
+# TODO:
+# Read cherry_pick.txt fajl for z-vectors, each line is one vector (see file for format)
+# For each vector, generate image using wgan.generate
+# Make version of dataset.get_nearest_neighbour that returns list of file names
+# Find neighbours
+# Save list of found names in cherry_pick_neighbour.txt
---
+++ src/main_neighbour_vis.py
@@ -0,0 +1,5 @@
+# TODO:
+# Read vectors from cherry_pick.txt and generate images
+# Read cherry_pick_neighbour.txt for images files and load them
+# Merge two array of images and show them as grid binary
+# See wgan.generate_random_with_neighbor for how to visualize two column grid
